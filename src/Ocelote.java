a002bffbc442f537afe5fc9428bd3c40895f95d0
SanguchoMela <melany01-03@hotmail.com>
2023-06-20 10:01:19 -0500

    clase hija Ocelote completa

--- src/Ocelote.java
+++ src/Ocelote.java
@@ -1,2 +1,37 @@
-public class Ocelote extends Felinos{
+public class Ocelote extends Felinos {
+    private String manchasDistintivas;
+    private String territorio;
+
+    public Ocelote(){
+        super();
+    }
+
+    public Ocelote(int subEspecie, String nombre, int edad, double tamanio, double peso, String manchasDistintivas, String territorio) {
+        super(subEspecie, nombre, edad, tamanio, peso);
+        this.manchasDistintivas = manchasDistintivas;
+        this.territorio = territorio;
+    }
+
+    public String getManchasDistintivas() {
+        return manchasDistintivas;
+    }
+
+    public void setManchasDistintivas(String manchasDistintivas) {
+        this.manchasDistintivas = manchasDistintivas;
+    }
+
+    public String getTerritorio() {
+        return territorio;
+    }
+
+    public void setTerritorio(String territorio) {
+        this.territorio = territorio;
+    }
+
+    private void caminarSinRuido(){
+        System.out.println("Este es el metodo para caminar sin hacer ruido");
+    }
+    private void treparArboles(){
+        System.out.println("Este es el metodo para trepar arboles");
+    }
 }
