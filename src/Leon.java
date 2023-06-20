4c659f0e3704c92735efd6d70e4d2d2dfe6b066d
SanguchoMela <melany01-03@hotmail.com>
2023-06-20 10:11:01 -0500

    clase hija Leon completa

--- src/Leon.java
+++ src/Leon.java
@@ -1,2 +1,47 @@
 public class Leon extends Felinos{
+    private String melena;
+    private String rugido;
+    private String manada;
+
+    public Leon (){
+        super();
+    }
+
+    public Leon(int subEspecie, String nombre, int edad, double tamanio, double peso, String melena, String rugido, String manada) {
+        super(subEspecie, nombre, edad, tamanio, peso);
+        this.melena = melena;
+        this.rugido = rugido;
+        this.manada = manada;
+    }
+
+    public String getMelena() {
+        return melena;
+    }
+
+    public void setMelena(String melena) {
+        this.melena = melena;
+    }
+
+    public String getRugido() {
+        return rugido;
+    }
+
+    public void setRugido(String rugido) {
+        this.rugido = rugido;
+    }
+
+    public String getManada() {
+        return manada;
+    }
+
+    public void setManada(String manada) {
+        this.manada = manada;
+    }
+
+    private void cazarGrupal(){
+        System.out.println("Este es el metodo para cazar en grupo");
+    }
+    private void liderarManada(){
+        System.out.println("Este es el metodo para liderar la manada");
+    }
 }
