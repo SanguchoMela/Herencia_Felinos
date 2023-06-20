7b1b4755f1fd74a27a9926ad7625ac0985ad4fd7
SanguchoMela <melany01-03@hotmail.com>
2023-06-20 10:07:53 -0500

    clase hija Irbis completa

--- src/Irbis.java
+++ src/Irbis.java
@@ -1,2 +1,40 @@
 public class Irbis extends Felinos{
+    private String pelaje;
+    private String presas;
+
+    public Irbis(){
+        super();
+    }
+
+    public Irbis(int subEspecie, String nombre, int edad, double tamanio, double peso, String pelaje, String presas) {
+        super(subEspecie, nombre, edad, tamanio, peso);
+        this.pelaje = pelaje;
+        this.presas = presas;
+    }
+
+    public String getPelaje() {
+        return pelaje;
+    }
+
+    public void setPelaje(String pelaje) {
+        this.pelaje = pelaje;
+    }
+
+    public String getPresas() {
+        return presas;
+    }
+
+    public void setPresas(String presas) {
+        this.presas = presas;
+    }
+
+    private void caminarNieve(){
+        System.out.println("Este es el metodo para caminar sobre la nieve");
+    }
+    private void escalarRocas(){
+        System.out.println("Este es el metodo para escalar rocas");
+    }
+    private void adaptarFrio(){
+        System.out.println("Este es el metodo para adaptarse al frio");
+    }
 }
